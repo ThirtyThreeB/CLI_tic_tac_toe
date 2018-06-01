3a57e22c9fe5529809e182902f6f6a70a4aa1804
Gabriel Aragon <thirtythreebananas@gmail.com>
2018-06-01 09:57:12 -0400

    Reworked game logic outside of the object

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -6,7 +6,10 @@ class TicTacToe
 	def initialize 
 		@score = {"X" => 0, "O" => 0}
 		@round = 1
-		start
+		
+		puts "Play by entering the number\nthat corresponds with the space you choose"
+		#puts @board
+		# start
 	end
 
 	def start
@@ -15,23 +18,19 @@ class TicTacToe
 		@o_array = []
 		@available_spaces = [*1..9]
 		@board = "   1 2 3\n   4 5 6\n   7 8 9"
-
-		if @round == 1
-			puts @board
-			puts "Here is the gameboard,\nyou'll play by entering the number\nthat corresponds with the space you choose"
-			choose_team
-		else 
-			puts "Let's play round #{@round}"
-		end	
-		whose_turn
+		@winner = false
+		# puts "Let's play round #{@round}"
+		#whose_turn
 	end
 
-	def choose_team 
-		puts "You are the first player, do you want to be X's or O's ?"
-		x_or_o
+	def new_round
+		new_round = false
 	end
+
 		
 	def x_or_o #validate choice of team either x or o
+		team_choice = nil
+		puts "You are the first player, do you want to be X's or O's ?" if team_choice.nil?
 		team_choice = gets.chomp.upcase
 		if team_choice == 'X' || team_choice == 'O' 
 		  puts "Cool, you're on team #{team_choice}" 
@@ -42,74 +41,87 @@ class TicTacToe
 		end
 	end
 
-	def whose_turn 
-
+	def whose_turn # alternates which player starts each round
 		if @round%2 == 1 # X's starting round when odd
 			@first_player, @second_player = "X", "O"
 		else
 			@first_player, @second_player = "O", "X"
-		end
-
-		@turn%2 == 1 ? play_turn(@first_player) : play_turn(@second_player) #switches between player x and player o
+		end	
 	end
 
-	def play_turn(player) 
-		puts "Player #{player} goes first this round" unless @turn > 1
-		puts "Player #{player} choose a space"
+	# def switch_turn # switches players every turn
+	# 	#@turn%2 == 1 ? play_turn(@first_player) : play_turn(@second_player) 
+	# end
+
+	def play_turn
+		puts "Let's play round #{@round}" if @round > 1 && @turn == 0
+		@turn%2 == 1 ? @player = @first_player : @player = @second_player
+		puts "Player #{@player} goes first this round" unless @turn > 1
+		puts "Player #{@player} choose a space"
 		puts @board
-		val_choice(player)
+		
+		# val_choice(player)
 		@turn += 1
-		end_game 
-		whose_turn
+		#player
 	end
 
-	def val_choice(player) #make sure only 1-9, or whatever is left is entered
-		choice = gets.chomp
-		if @available_spaces.include?(choice.to_i) 
-			@board = @board.gsub(choice, player)
+	def val_choice #make sure only 1-9, or whatever is left is entered
+		@choice = gets.chomp
+		if @available_spaces.include?(@choice.to_i) 
+			@board = @board.gsub(@choice, @player)
 			puts @board
-			chosen_spaces(choice, player)
-			@available_spaces.delete(choice.to_i)
+			#return choice, player
+			# chosen_spaces(choice, player) #not sure how to get choice, player to return so I can pull this method out to the game
+			@available_spaces.delete(@choice.to_i)
 		else
 			puts "Nope, enter an available space.  Try again" 
-			val_choice(player)
+			val_choice
 		end
 	end
 
-	def chosen_spaces(choice, player) #this chooses which array to push the chose space to depending on whose turn it is
-		if player == "X"
-			@x_array << choice.to_i
-			else
-			@o_array << choice.to_i
+	def chosen_spaces #this chooses which array to push the chose space to depending on whose turn it is
+		if @player == "X"
+			@x_array << @choice.to_i
+		else
+			@o_array << @choice.to_i
 		end
 	end
 
-	def end_game #check to see if any winning patterns, or if board is full then 'tie'
+	def round_won #check to see if any winning patterns, or if board is full then 'tie'
 		WINNING_PATTERNS.each do |pattern|
-			if pattern- @x_array == []
+			if pattern - @x_array == []
 		 		puts "X wins!" 
 		 		@score["X"] += 1
-		 		reset
+		 		@winner = true
+		 		return
 		 	end
-		  if pattern- @o_array == []
+		  if pattern - @o_array == []
 		 		puts "O wins!" 
 		 		@score["O"] += 1
-		 		reset
+		 		@winner = true
+		 		return
 		  end
 		end
-		if @turn == 9 
+		if @turn == 9 #&& @winner = false
 			puts "Cat's game"  
-			reset
+			@winner = true
 		end
+		p @turn
 	end
 
-	def reset #clear board
-		puts "The score is Player X- #{@score["X"]}, Player O- #{@score["O"]}"
-		@round += 1
-		play_again
+	def update_score #clear board
+		if @winner
+			puts "The score is Player X- #{@score["X"]}, Player O- #{@score["O"]}" 
+			@round += 1
+		end
+		# play_again
+	end
+
+	def round_is_over?
+		@winner ? true : false
 	end
 	
-	def play_again
+	def play_again #after every round
 		puts "That was awesome! You want to keep playing right? Enter y/n"
 		keep_playing = gets.chomp
 		while keep_playing != "y" && keep_playing != "n"
@@ -119,14 +131,42 @@ class TicTacToe
 
 		if keep_playing == "y"
 			puts "Sweet, I bet this next round will be even more fun!" 
-		  start 
+		  @game_over = false
 		else
 			puts "Alright. Well, good luck finding something better to do. "  
-		  exit
+		  @game_over = true
 		end
+	end
+
+
+
+	def is_over?
+		@game_over
+	end
+
+	def good_bye
+		puts "Thanks for playing"
+		exit
 	end
 
 end
 
-TicTacToe.new
-
+game = TicTacToe.new
+#game.start
+game.x_or_o
+while !game.is_over?
+	round = game
+	game.start
+	while !game.round_is_over?
+		game.whose_turn
+		game.play_turn
+		game.val_choice
+		game.chosen_spaces
+		game.round_won
+		game.update_score
+		game.round_is_over?
+	end
+	game.new_round
+	game.play_again
+end		
+game.good_bye
